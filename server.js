0f1cca87456716823eb4100b71318803cf8d2f7f
Colin Rent <colinrent@Colins-MacBook-Pro.local>
2024-02-17 12:52:41 -0500

    added to server.js and .gitignore

---
+++ server.js
@@ -0,0 +1,17 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'employee_db'
+    });
